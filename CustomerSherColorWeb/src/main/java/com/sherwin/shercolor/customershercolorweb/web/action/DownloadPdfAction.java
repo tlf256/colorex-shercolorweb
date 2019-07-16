70cba713558b785e4a8e03a069109f7d1d2b1130
Eric Oswald <eric.oswald@sherwin.com>
2019-07-16 10:56:56 -0400

    Needed for Help Menu links

---
+++ CustomerSherColorWeb/src/main/java/com/sherwin/shercolor/customershercolorweb/web/action/DownloadPdfAction.java
@@ -0,0 +1,67 @@
+package com.sherwin.shercolor.customershercolorweb.web.action;
+
+import java.util.Map;
+
+import org.apache.struts2.interceptor.SessionAware;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+
+@SuppressWarnings("serial")
+public class DownloadPdfAction extends ActionSupport  implements SessionAware, LoginRequired  {
+
+	static Logger logger = LogManager.getLogger(DownloadPdfAction.class);
+	private Map<String, Object> sessionMap;
+	private String reqGuid;
+	private String pdfFile;
+	
+	private InputStream fileInputStream;
+
+	public InputStream getFileInputStream() {
+		return fileInputStream;
+	}
+
+	public String execute() throws Exception {
+		try {
+			fileInputStream = new FileInputStream(new File("/web_apps/server/shercolor/external/" + pdfFile));
+	    	return SUCCESS;
+		 } catch (Exception e) {
+			logger.error(e.getMessage());
+			return ERROR;
+		}
+	}
+
+	public void setSession(Map<String, Object> sessionMap) {
+		this.setSessionMap(sessionMap);
+	}
+
+	public Map<String, Object> getSessionMap() {
+		return sessionMap;
+	}
+
+	public void setSessionMap(Map<String, Object> sessionMap) {
+		this.sessionMap = sessionMap;
+	}
+
+	public String getReqGuid() {
+		return reqGuid;
+	}
+
+	public void setReqGuid(String reqGuid) {
+		this.reqGuid = reqGuid;
+	}
+
+	public String getPdfFile() {
+		return pdfFile;
+	}
+
+	public void setPdfFile(String pdfFile) {
+		this.pdfFile = pdfFile;
+	}
+}
+
