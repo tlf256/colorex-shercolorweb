4002b05ef0ff65b5cfc583119429ddb55037933b
Eric Oswald <eric.oswald@sherwin.com>
2019-08-09 10:07:59 -0400

    Changes to the Help Menu download names

--- CustomerSherColorWeb/src/main/java/com/sherwin/shercolor/customershercolorweb/web/action/DownloadPdfAction.java
+++ CustomerSherColorWeb/src/main/java/com/sherwin/shercolor/customershercolorweb/web/action/DownloadPdfAction.java
@@ -32,28 +32,28 @@ public class DownloadPdfAction extends ActionSupport  implements SessionAware, L
 		String pdfFileName = "";
 		try {
 			switch(pdfFile) {
-				case "1":
+				case "User Guide":
 					pdfFileName = "SherColor_Web_Customer_Guide.pdf";
 					break;
-				case "2":
+				case "Setting Up Your Accutinter":
 					pdfFileName = "SherColor_Web_Accutinter_Installation_Guide.pdf";
 					break;
-				case "3":
+				case "Calibrating Your Accutinter":
 					pdfFileName = "SherColor_Web_Fluid_Management_Calibration.pdf";
 				    break;
-				case "4":
+				case "Installing Your Color Eye":
 				    pdfFileName = "SherColor_Web_Color_Eye_Installation.pdf";
 				    break;
-				case "5":
+				case "Setting Up Your Corob":
 				    pdfFileName = "SherColor_Web_Corob_Installation_Guide.pdf";
 				    break;
-				case "6":
+				case "Calibrating Your Corob":
 				    pdfFileName = "SherColor_Web_Corob_Calibration.pdf";
 				    break;
-				case "7":
+				case "Installing Your Dymo Printer":
 				    pdfFileName = "SherColor_Web_Dymo_Install.pdf";
 				    break;
-				case "8":
+				case "Installing Your Zebra Printer":
 				    pdfFileName = "SherColor_Web_Zebra_Install.pdf";
 				    break;
 				default:
