9a687bb8a04f5ba0d52d6f9297b97f0eb201a5d0
djm301 <donald.j.miller@sherwin.com>
2020-03-31 16:06:07 -0400

    junit for TinterEventAction - PSCWEB-197

---
+++ CustomerSherColorWeb/src/test/java/com/sherwin/shercolor/customershercolorweb/web/action/TinterEventActionTest.java
@@ -0,0 +1,159 @@
+package com.sherwin.shercolor.customershercolorweb.web.action;
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+import org.apache.struts2.StrutsSpringTestCase;
+
+import org.junit.Test;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import com.google.gson.Gson;
+import com.opensymphony.xwork2.ActionProxy;
+
+
+import com.sherwin.shercolor.customershercolorweb.web.action.TinterEventAction;
+import com.sherwin.shercolor.customershercolorweb.web.model.RequestObject;
+import com.sherwin.shercolor.customershercolorweb.web.model.TinterInfo;
+
+
+@Transactional
+
+public class TinterEventActionTest extends StrutsSpringTestCase{
+
+	TinterEventAction target;
+	RequestObject reqObj = new RequestObject();
+	String reqGuid = "12345";	
+
+	@Override
+	public String[] getContextLocations() {
+		String[] arrStr =  {
+				"classpath:config/spring/shercolorcommon.xml"
+
+		} ;  
+		return arrStr;
+
+	}
+
+	@Test
+	public void testTinterEventsAction(){
+		ActionProxy proxy = getActionProxy("/logTinterEventAction");
+		TinterInfo tinter = new TinterInfo();
+		Map<String,Object> tinterMessage = new HashMap<String,Object>();
+		
+		target = (TinterEventAction) proxy.getAction();
+
+		target.setReqGuid("12345");
+
+
+
+		reqObj.setCustomerID("TEST");
+
+		request.setParameter("reqGuid",reqGuid);
+		HttpSession session = request.getSession();
+		session.setAttribute(reqGuid, reqObj);
+
+		tinter.setClrntSysId("CCE");
+		tinter.setModel("FM UNITTEST");
+		tinter.setSerialNbr("TESTFMSERIAL");
+		reqObj.setTinter(tinter);
+		
+		
+		
+		tinterMessage.put("command", "TestTinter");
+		tinterMessage.put("errorNumber", "88");
+		tinterMessage.put("errorSeverity", "8");
+		target.setTinterMessage(tinterMessage);
+
+		try {
+			String json = executeAction("/logTinterEventAction");
+			
+			ProcessTinterConfigAction result = new Gson().fromJson(json,ProcessTinterConfigAction.class);
+			//String result = proxy.execute();
+			assertEquals("12345",result.getReqGuid());
+
+			
+			//System.out.println("Got my Cal for " + cal.getId());
+		}
+		catch (Exception e) {
+			// TODO Auto-generated catch block
+			try {
+				throw(e.getCause());
+			} catch (Throwable e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			//e.printStackTrace();
+		}	
+		//String theResult = target.execute();
+		//assertEquals("SUCCESS",theResult);
+	}
+	@Test
+	public void testTinterEventsConfigAction(){
+		ActionProxy proxy = getActionProxy("/logTinterEventConfigAction");
+		TinterInfo tinter = new TinterInfo();
+		Map<String,Object> tinterMessage = new HashMap<String,Object>();
+		
+		target = (TinterEventAction) proxy.getAction();
+
+		target.setReqGuid("12345");
+
+
+
+		reqObj.setCustomerID("TEST");
+
+		request.setParameter("reqGuid",reqGuid);
+		HttpSession session = request.getSession();
+		session.setAttribute(reqGuid, reqObj);
+
+		tinter.setClrntSysId("CCE");
+		tinter.setModel("COROB UNITTEST");
+		tinter.setSerialNbr("TESTSERIAL");
+		target.setNewTinter(tinter);
+		
+		
+		tinterMessage.put("command", "TestConfig");
+		tinterMessage.put("errorNumber", "99");
+		tinterMessage.put("errorSeverity", "9");
+		target.setTinterMessage(tinterMessage);
+
+		
+
+		try {
+			String json = executeAction("/logTinterEventConfigAction");
+			
+			ProcessTinterConfigAction result = new Gson().fromJson(json,ProcessTinterConfigAction.class);
+			//String result = proxy.execute();
+			assertEquals("12345",result.getReqGuid());
+
+			
+			//System.out.println("Got my Cal for " + cal.getId());
+		}
+		catch (Exception e) {
+			// TODO Auto-generated catch block
+			try {
+				throw(e.getCause());
+			} catch (Throwable e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			//e.printStackTrace();
+		}	
+		
+			
+	}	
+
+
+}
