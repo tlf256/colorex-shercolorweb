5425c66e85a1fe1ec5f8db1d15be9988ffb4c62c
Daniel Adams <daniel.j.adams@sherwin.com>
2022-01-18 08:46:49 -0500

    Updates

--- CustomerSherColorWeb/src/main/java/com/sherwin/shercolor/customershercolorweb/SherColorWebApplication.java
+++ CustomerSherColorWeb/src/main/java/com/sherwin/shercolor/customershercolorweb/SherColorWebApplication.java
@@ -1,5 +1,6 @@
 package com.sherwin.shercolor.customershercolorweb;
 
+
 import org.apache.struts2.dispatcher.filter.StrutsPrepareAndExecuteFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +8,9 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication(scanBasePackages = {"com.sherwin.shercolor","com.sherwin.login"})
 public class SherColorWebApplication extends SpringBootServletInitializer {
