f6d957492e62bb4a6930d00fc9eb4e090812db72
Daniel Adams <daniel.j.adams@sherwin.com>
2022-01-20 09:53:28 -0500

    Updating wildfly bootable config and cleaning up SherColorLogin for Spring Data JPA conversion

--- SherColorLogin/src/main/java/com/sherwin/login/service/SWUserCommentServiceImpl.java
+++ SherColorLogin/src/main/java/com/sherwin/login/service/SWUserCommentServiceImpl.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.sherwin.login.dao.SWUserCommentDao;
 import com.sherwin.login.domain.SWUserComments;
 
 
