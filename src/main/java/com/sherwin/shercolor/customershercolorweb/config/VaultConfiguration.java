0cc9089f697d7357af6dc493a0dec7ff1f65ed29
Daniel Adams <daniel.j.adams@sherwin.com>
2022-06-14 08:38:10 -0400

    Create VaultConfiguration.java

---
+++ src/main/java/com/sherwin/shercolor/customershercolorweb/config/VaultConfiguration.java
@@ -0,0 +1,12 @@
+package com.sherwin.shercolor.customershercolorweb.config;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+//Exclude Vault config normally, only used in certain use-cases such as local development and testing
+@Configuration
+@Profile("!local")
+@EnableAutoConfiguration(exclude = org.springframework.cloud.vault.config.VaultAutoConfiguration.class)
+public class VaultConfiguration {
+}
