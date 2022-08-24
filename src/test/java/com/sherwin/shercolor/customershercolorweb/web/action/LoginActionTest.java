5f4bd94af492c51e4e9de9874de421b6cd51b561
Terri Freeman <99204551+tlf256@users.noreply.github.com>
2022-08-24 14:48:28 -0400

    Modified LoginActionTest to assert result

--- src/test/java/com/sherwin/shercolor/customershercolorweb/web/action/LoginActionTest.java
+++ src/test/java/com/sherwin/shercolor/customershercolorweb/web/action/LoginActionTest.java
@@ -1,6 +1,11 @@
 package com.sherwin.shercolor.customershercolorweb.web.action;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.http.HttpSession;
 
@@ -11,6 +16,8 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.opensymphony.xwork2.Action;
+import com.opensymphony.xwork2.ActionProxy;
 import com.sherwin.shercolor.customershercolorweb.web.model.RequestObject;
 
 @Transactional
@@ -21,9 +28,9 @@ public class LoginActionTest extends StrutsSpringJUnit4TestCase<LoginAction> {
 	LoginAction target;
 	RequestObject reqObj = new RequestObject();
 	String guid1 = "12345";
-
+	
 	@Test
-	public void testLoginAction() {
+	public void testLoginAction_success() {
 		reqObj.setCustomerID("400000008");
 		reqObj.setUserId("shercolortest");
 		reqObj.setFirstName("SherColor");
@@ -32,17 +39,21 @@ public class LoginActionTest extends StrutsSpringJUnit4TestCase<LoginAction> {
 		reqObj.setHomeStore(9989);
 		reqObj.setTerritory("null");
 		
-		request.setParameter("guid1",guid1);
-		HttpSession session = request.getSession();
-		session.setAttribute(guid1, reqObj);
+		ActionProxy proxy = getActionProxy("/loginAction");
+        assertNotNull(proxy);
+        
+        target = (LoginAction) proxy.getAction();
+        target.setGuid1(guid1);
+
+        Map<String, Object> sessionMap = new HashMap<>();
+        sessionMap.put(guid1, reqObj);
+        
+        proxy.getInvocation().getInvocationContext().setSession(sessionMap);
 		
 		try {
-			executeAction("/loginAction");
-			
-			String reqGuid = (String) findValueAfterExecute("reqGuid");
-			//System.out.println("loginAction reqGuid is " + reqGuid);
-			assertTrue(reqGuid != null);
-			
+			String result = proxy.execute();
+			//System.out.println("loginAction result is " + result);
+			assertEquals(Action.SUCCESS, result);
 		} catch (Exception e) {
 			try {
 				throw(e.getCause());
