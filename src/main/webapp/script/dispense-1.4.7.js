20b0fc8f98f834f43b8a33ffe69e36d8af943e7a
Terri Freeman <99204551+tlf256@users.noreply.github.com>
2022-08-31 10:32:59 -0400

    Added Linux condition to FMXDispenseComplete

--- src/main/webapp/script/dispense-1.4.7.js
+++ src/main/webapp/script/dispense-1.4.7.js
@@ -276,7 +276,9 @@ function FMXDispenseComplete(return_message) {
 //	buildProgressBars(return_message);
 	$("#abort-message").hide();
 
-	if ((return_message.errorNumber == 0 && return_message.commandRC == 0) || (return_message.errorNumber == -10500 && return_message.commandRC == -10500)) {
+	if ((return_message.errorNumber == 0 && (platform.startsWith("Win") && return_message.commandRC == 0 
+		|| platform.startsWith("Lin") && return_message.commandRC == 2)) 
+		|| (return_message.errorNumber == -10500 && return_message.commandRC == -10500)) {
 		// save a dispense (will bump the counter)
 		getSessionTinterInfo($("#reqGuid").val(), warningCheck);
 
