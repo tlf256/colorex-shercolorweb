d383034a01950ded636746d23c8b9e1fd812ce58
Daniel Adams <daniel.j.adams@sherwin.com>
2023-01-17 15:29:05 -0500

    chore: adding test coverage

--- src/test/java/com/sherwin/shercolor/customershercolorweb/web/action/DownloadPdfActionTest.java
+++ src/test/java/com/sherwin/shercolor/customershercolorweb/web/action/DownloadPdfActionTest.java
@@ -1,30 +1,20 @@
 package com.sherwin.shercolor.customershercolorweb.web.action;
 
+import com.opensymphony.xwork2.Action;
+import com.opensymphony.xwork2.ActionProxy;
 import com.sherwin.shercolor.customershercolorweb.annotation.SherColorWebTransactionalTest;
 import com.sherwin.shercolor.customershercolorweb.web.model.RequestObject;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.http.entity.ContentType;
 import org.apache.struts2.StrutsSpringJUnit4TestCase;
-import org.junit.Before;
-import org.junit.ClassRule;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.junit4.rules.SpringClassRule;
-import org.springframework.test.context.junit4.rules.SpringMethodRule;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
-@RunWith(Parameterized.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 @SherColorWebTransactionalTest
 public class DownloadPdfActionTest extends StrutsSpringJUnit4TestCase<DownloadPdfAction> {
 
@@ -32,51 +22,30 @@ public class DownloadPdfActionTest extends StrutsSpringJUnit4TestCase<DownloadPd
 	private static final String reqGuid = "12345";
 	private static final String customerId = "678910";
 
-	private final String filename;
-
-	public DownloadPdfActionTest(String filename) {
-		this.filename = filename;
-	}
-
-	@ClassRule
-	public static final SpringClassRule SPRING_CLASS_RULE
-			= new SpringClassRule();
-
-	@Rule
-	public final SpringMethodRule springMethodRule
-			= new SpringMethodRule();
-
-	@Parameterized.Parameters
-	public static List<String> data() {
-		return Arrays.asList("SherColor_Web_Customer_Guide",
-				"SherColor_Web_XProtint_Tinter_Installation_Guide",
-				"SherColor_Web_Accutinter_Installation_Guide",
-				"SherColor_Web_Fluid_Management_Calibration",
-				"SherColor_Web_Color_Eye_Installation",
-				"SherColor_Web_Corob_Installation_Guide",
-				"SherColor_Web_Corob_Calibration",
-				"SherColor_Web_Dymo_Install",
-				"SherColor_Web_Zebra_Install");
-	}
 
 	@Test
-	public void execute_NoInputs_ProperPdfReturned() throws Exception {
+	public void execute_NoInputsUnhandledPdfType_ErrorReturned() {
 
 		//arrange
-		request.setParameter("reqGuid",reqGuid);
-		request.setParameter("pdfType",filename);
+		// get action proxy and action object
+		ActionProxy proxy = getActionProxy("/downloadPdfAction.action");
+		assertNotNull(proxy);
+		DownloadPdfAction action = (DownloadPdfAction) proxy.getAction();
+		assertNotNull(action);
+
+		// set sessionMap into the action's session
+		Map<String, Object> sessionMap = new HashMap<>();
 		reqObj.setCustomerID(customerId);
-		Objects.requireNonNull(request.getSession()).setAttribute(reqGuid, reqObj);
+		sessionMap.put(reqGuid, reqObj);
+		action.setSession(sessionMap);
+		action.setReqGuid(reqGuid);
+		action.setPdfType("woah");
 
 		//act
-		String json = executeAction("/downloadPdfAction.action");
+		String result = action.execute();
 
 		//assert
-		assertTrue(StringUtils.isNotBlank(json));
-		assertEquals(response.getContentType(), ContentType.APPLICATION_OCTET_STREAM.getMimeType());
-		String contentDisposition = response.getHeader("Content-Disposition");
-		assertTrue(StringUtils.isNotBlank(contentDisposition));
-		assertTrue(StringUtils.contains(contentDisposition,filename));
-		assertTrue(StringUtils.contains(contentDisposition,".pdf"));
+		assertTrue(StringUtils.isNotBlank(result));
+		assertEquals(Action.ERROR, result);
 	}
 }
