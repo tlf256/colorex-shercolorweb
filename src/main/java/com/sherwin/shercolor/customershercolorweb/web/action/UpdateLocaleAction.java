5ae341cd599bd1feccbbb977f6fae1f2cd74471c
Daniel Adams <daniel.j.adams@sherwin.com>
2023-04-05 13:12:54 -0400

    fix: adding unsupported locale validation

--- src/main/java/com/sherwin/shercolor/customershercolorweb/web/action/UpdateLocaleAction.java
+++ src/main/java/com/sherwin/shercolor/customershercolorweb/web/action/UpdateLocaleAction.java
@@ -10,6 +10,8 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.struts2.ServletActionContext;
+import org.apache.struts2.StrutsStatics;
+import org.apache.struts2.interceptor.I18nInterceptor;
 import org.apache.struts2.interceptor.ServletRequestAware;
 import org.apache.struts2.interceptor.ServletResponseAware;
 import org.apache.struts2.interceptor.SessionAware;
@@ -22,45 +24,44 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class UpdateLocaleAction extends ActionSupport implements SessionAware, ServletResponseAware, ServletRequestAware {
-	
-    private static final long serialVersionUID = 1L;
+
+	private static final long serialVersionUID = 1L;
     static Logger logger = LogManager.getLogger(UpdateLocaleAction.class);
     private Map<String, Object> sessionMap;
     private HttpServletResponse response;
     private HttpServletRequest request;
-	
-    @Autowired
+
+	@Autowired
     LocaleContainer localeContainer;
-	
-   
-    
-    public String execute() {
-    	try {
-    		logger.info("begin execute...");
-	    	Locale userLocale = (Locale) sessionMap.get("WW_TRANS_I18N_LOCALE");
-	    	// userLocale will be null if user has not requested a language change during this session
-	    	if (userLocale != null) {
-	    		localeContainer.setLocale(userLocale);
-	    		
-	    		// convert locale to language tag so it can be easily converted back later
-	    		String localeId = userLocale.toLanguageTag();
-	    		
-	    		// check for consent cookie before creating locale cookie
-	    		if(consentCookieExists()) {
-	    			logger.info("consent cookie exists, creating locale cookie");
-	    			Cookie localeCookie = new Cookie("locale", localeId);
-		    		localeCookie.setMaxAge(60 * 60 * 24 * 365 * 10);
-		    		// httponly helps to prevent XSS attacks
-		    		localeCookie.setHttpOnly(true);
-		    		response = (HttpServletResponse) ActionContext.getContext().get(ServletActionContext.HTTP_RESPONSE);
-		    		response.addCookie(localeCookie);
-	    		}
-	    		
-	    	}
-	    	
-	    	return SUCCESS;
-    	
-    	} catch (RuntimeException e) {
+
+
+	public String execute() {
+		try {
+			logger.info("begin execute...");
+			Locale userLocale = (Locale) sessionMap.get(I18nInterceptor.DEFAULT_SESSION_ATTRIBUTE);
+			// userLocale will be null if user has not requested a language change during this session
+			if (userLocale != null) {
+				localeContainer.setLocale(userLocale);
+
+				// convert locale to language tag so it can be easily converted back later
+				String localeId = userLocale.toLanguageTag();
+
+				// check for consent cookie before creating locale cookie
+				if(consentCookieExists()) {
+					logger.info("consent cookie exists, creating locale cookie");
+					Cookie localeCookie = new Cookie("locale", localeId);
+					localeCookie.setMaxAge(60 * 60 * 24 * 365 * 10);
+					// httponly helps to prevent XSS attacks
+					localeCookie.setHttpOnly(true);
+					response = (HttpServletResponse) ActionContext.getContext().get(StrutsStatics.HTTP_RESPONSE);
+					response.addCookie(localeCookie);
+				}
+
+			}
+
+			return SUCCESS;
+
+		} catch (RuntimeException e) {
 			logger.error("Exception Caught: " + e.getMessage(), e);
 			return ERROR;
 		}
@@ -68,44 +69,44 @@ public class UpdateLocaleAction extends ActionSupport implements SessionAware, S
     
     
     public String checkLocaleCookie() {
-    	try {
-	    	String cookieLocale = null;
+		try {
+			String cookieLocale = null;
 			request = (HttpServletRequest) ActionContext.getContext().get(ServletActionContext.HTTP_REQUEST);
 			if(request != null && request.getCookies() != null) {
 				for (Cookie c : request.getCookies()) {
-		            if (c.getName().equals("locale")) {
-		                cookieLocale = c.getValue();
-		            }
-		        }
+					if (c.getName().equals("locale")) {
+						cookieLocale = c.getValue();
+					}
+				}
 			}
 			if (cookieLocale != null) {
 				Locale userLocale = Locale.forLanguageTag(cookieLocale);
 				if (userLocale != null) {
 					localeContainer.setLocale(userLocale);
-					sessionMap.put("WW_TRANS_I18N_LOCALE", userLocale);
+					sessionMap.put(I18nInterceptor.DEFAULT_SESSION_ATTRIBUTE, userLocale);
 				}
-			} 	
-	    	
-	    	return SUCCESS;
-    	
-    	} catch (RuntimeException e) {
+			}
+
+			return SUCCESS;
+
+		} catch (RuntimeException e) {
 			logger.error("Exception Caught: " + e.getMessage(), e);
 			return ERROR;
 		}
     }
     
     private boolean consentCookieExists() {
-    	logger.info("consentCookieExists: getting request and checking for consent");
-    	request = (HttpServletRequest) ActionContext.getContext().get(ServletActionContext.HTTP_REQUEST);
+		logger.info("consentCookieExists: getting request and checking for consent");
+		request = (HttpServletRequest) ActionContext.getContext().get(ServletActionContext.HTTP_REQUEST);
 		Cookie[] cookies = null;
 		String cookieName = "";
 		boolean result = false;
-		
+
 		if(request != null) {
 			logger.debug("consentCookieExists: request is not null");
 			cookies = request.getCookies();
 		}
-		
+
 		// verify if consent has already been given
 		if(cookies != null) {
 			logger.debug("consentCookieExists: cookies array is not null");
@@ -117,7 +118,7 @@ public class UpdateLocaleAction extends ActionSupport implements SessionAware, S
 				}
 			}
 		}
-		
+
 		return result;
     }
     
@@ -133,13 +134,13 @@ public class UpdateLocaleAction extends ActionSupport implements SessionAware, S
     public void setSessionMap(Map<String, Object> sessionMap) {
         this.sessionMap = sessionMap;
     }
-    
+
 
 	@Override
 	public void setServletResponse(HttpServletResponse response) {
 		this.response = response;
 	}
-	
+
 	@Override
 	public void setServletRequest(HttpServletRequest request) {
 		this.request = request;
